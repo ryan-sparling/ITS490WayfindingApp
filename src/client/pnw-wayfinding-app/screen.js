1cc5d7f42adbc7da8e7c184a89ba0e64d34f5dc7
ryan-sparling <rsparling1992@gmail.com>
2021-02-12 15:12:36 -0600

    version 1.0.3

--- src/client/pnw-wayfinding-app/screen.js
+++ src/client/pnw-wayfinding-app/screen.js
@@ -1,33 +1,62 @@
 import React from "react";
 import {
+  Beacons,
   Container,
+  Listview,
   VBox,
   BannerHeader,
   BasicSegment,
+  screenLink,
+  Button, 
+  ButtonGroup,
+  Divider,
+  Field,
+  AddressPicker,
+  Message,
+  Header,
+  NavMenu,
+  NavMenuItem,
+  AekReactRouter,
+  RouterView
 } from "@ombiel/aek-lib";
+
+
+const url = screenLink("campusm://menu?menucode=1000005687")
+const beacons = new Beacons({includePos: "N"});
+
+beacons.on("data", (data) => {
+  setBeaconData(data);
+});
 
 
 export default class Screen extends React.Component {
 
   componentDidMount() {
-
+    
   }
 
   render() {
 
     return (
+      
+      
       <Container>
         <VBox>
           <BannerHeader theme="alt" key="header" data-flex={0}>PNW Wayfinding App</BannerHeader>
-          <BasicSegment>
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-            <p>Sed posuere consectetur est at lobortis.</p>
-          </BasicSegment>
-        </VBox>
-      </Container>
-    );
 
+          <Button href="campusm://menu?menucode=1000005687">Click Me</Button>
+
+          <Field label="My Input"/>
+          
+          
+          
+        </VBox>
+        
+
+      
+      </Container>
+      
+    )
   }
 
 }
