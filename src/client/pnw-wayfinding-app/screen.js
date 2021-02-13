d9986e2778976ba4a11bca6917d47ffd84e2ed63
ryan-sparling <rsparling1992@gmail.com>
2021-02-13 13:43:40 -0600

    beacon data added

--- src/client/pnw-wayfinding-app/screen.js
+++ src/client/pnw-wayfinding-app/screen.js
@@ -22,13 +22,71 @@ import {
 
 
 const url = screenLink("campusm://menu?menucode=1000005687")
-const beacons = new Beacons({includePos: "N"});
-
-beacons.on("data", (data) => {
-  setBeaconData(data);
-});
+const beacons = new Beacons({includePos: "Y"});
 
 
+let data = 
+[
+  {
+    uuid: "3cfc4026-5e81-4b76-a682-d54b7e252d12",
+    major : 34,
+    minor : 402,
+    rssi : -72,
+    positions : //Only exists if includePos is "Y"
+    [ 
+      {
+        posCode : "42",
+        locRef : "ref",
+        desc : "Harrison House",
+        note : "This is the house on Harrison",
+        //img : "https://portal.campusm.exlibrisgroup.com/assets/image.jpg",
+        mapCode : "10",
+        locCode : "6",
+        locCatDesc : "Buildings",
+        houseName : "Harrison House",
+        flat : "Flat 1",
+        streetName : "Harrison",
+        district : "University Quarter",
+        town : "Lowell",
+        county : "Indiana",
+        postCode : "46356",
+        showOnMap : true,
+        longitude : "-87.405738",
+        latitude : "41.293544"   
+      }
+    ]
+  },
+
+  {
+    uuid: "3cfc4026-5e81-4b76-a682-d54b7e252d12",
+    major : 34,
+    minor : 401,
+    rssi : -72,
+    positions : //Only exists if includePos is "Y"
+    [ 
+      {
+        posCode : "43",
+        locRef : "ref1",
+        desc : "Harrison Backyard",
+        note : "This is the backyard of the house on Harrison",
+        //img : "https://portal.campusm.exlibrisgroup.com/assets/image.jpg",
+        mapCode : "10",
+        locCode : "6",
+        locCatDesc : "Buildings",
+        houseName : "Harrison House",
+        flat : "Flat back",
+        streetName : "Harrison",
+        district : "University Quarter",
+        town : "Lowell",
+        county : "Indiana",
+        postCode : "46356",
+        showOnMap : true,
+        longitude : "-87.405955",
+        latitude : "41.293681"   
+      }
+    ]
+  }
+]
 export default class Screen extends React.Component {
 
   componentDidMount() {
@@ -36,6 +94,10 @@ export default class Screen extends React.Component {
   }
 
   render() {
+
+    beacons.on("data", (data) => {
+      setBeaconData(data);
+    });
 
     return (
       
