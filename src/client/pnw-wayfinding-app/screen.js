9853cf4425ef2305e7453f8e88d887ac6662539a
ryan-sparling <rsparling1992@gmail.com>
2021-02-19 15:17:34 -0600

    Beacon data Update Added beacon data Beacon Data added

--- src/client/pnw-wayfinding-app/screen.js
+++ src/client/pnw-wayfinding-app/screen.js
@@ -1,4 +1,13 @@
-import React from "react";
+/*
+Notes:
+
+2-15
+A fair amount of the body code is me messing around trying to figure out what I'm doing with react
+
+RRS
+*/
+
+import React, {useState} from "react";
 import {
   Beacons,
   Container,
@@ -17,109 +26,214 @@ import {
   NavMenu,
   NavMenuItem,
   AekReactRouter,
-  RouterView
+  RouterView,
+  Notifier,
+  Form,
+  Panel,
+  getPlatform
 } from "@ombiel/aek-lib";
 
+const Screen = () => {
+  const [disabled] = useState(getPlatform() === "web2");
+  const [loading, setLoading] = useState(false);
+  const [beaconData, setBeaconData] = useState({});
+  const [includePos, setIncludePos] = useState("Y");
+  
+  const getLocation = () => {
+    setLoading(true);
+    const beacons = new Beacons({includePos: includePos});
 
-const url = screenLink("campusm://menu?menucode=1000005687")
-const beacons = new Beacons({includePos: "Y"});
+    beacons.on("data", (res) => {
+      setBeaconData(res);
+      setLoading(false);
+    }); 
+  };
 
-
-let data = 
-[
-  {
-    uuid: "3cfc4026-5e81-4b76-a682-d54b7e252d12",
-    major : 34,
-    minor : 402,
-    rssi : -72,
-    positions : //Only exists if includePos is "Y"
-    [ 
-      {
-        posCode : "42",
-        locRef : "ref",
-        desc : "Harrison House",
-        note : "This is the house on Harrison",
-        //img : "https://portal.campusm.exlibrisgroup.com/assets/image.jpg",
-        mapCode : "10",
-        locCode : "6",
-        locCatDesc : "Buildings",
-        houseName : "Harrison House",
-        flat : "Flat 1",
-        streetName : "Harrison",
-        district : "University Quarter",
-        town : "Lowell",
-        county : "Indiana",
-        postCode : "46356",
-        showOnMap : true,
-        longitude : "-87.405738",
-        latitude : "41.293544"   
-      }
-    ]
-  },
-
-  {
-    uuid: "3cfc4026-5e81-4b76-a682-d54b7e252d12",
-    major : 34,
-    minor : 401,
-    rssi : -72,
-    positions : //Only exists if includePos is "Y"
-    [ 
-      {
-        posCode : "43",
-        locRef : "ref1",
-        desc : "Harrison Backyard",
-        note : "This is the backyard of the house on Harrison",
-        //img : "https://portal.campusm.exlibrisgroup.com/assets/image.jpg",
-        mapCode : "10",
-        locCode : "6",
-        locCatDesc : "Buildings",
-        houseName : "Harrison House",
-        flat : "Flat back",
-        streetName : "Harrison",
-        district : "University Quarter",
-        town : "Lowell",
-        county : "Indiana",
-        postCode : "46356",
-        showOnMap : true,
-        longitude : "-87.405955",
-        latitude : "41.293681"   
-      }
-    ]
+  let errorPara;
+  if (disabled) {
+    errorPara = <p className="error"> Browser does not support Beacons </p>;
   }
-]
-export default class Screen extends React.Component {
+  
+  return (
+    <Container>
+      <VBox>
+        <BannerHeader theme="alt" key="header" data-flex={0}> Beacons Test </BannerHeader>
+        <Panel>
+          <BasicSegment loading={loading}>
+            <Field 
+              label="Include Positions" 
+              type="select" 
+              size="mini" 
+              options={["Y", "N"]} 
+              name="includePos" 
+              onChange={(e) => { setIncludePos(e.target.value); }} 
+              value={includePos}
+            />
+            <Divider />
+            <Button 
+              onClick={getLocation} 
+              fluid 
+              variation="prime"
+              disabled={disabled}
+            >
+              Get Beacon Data
+            </Button>
+            <p style={{wordBreak: "break-all"}}> {JSON.stringify(beaconData)} </p>
+            {errorPara}
+          </BasicSegment>
+        </Panel>
+      </VBox>
+    </Container>
+  );
+};
+
+export default Screen;
+
+/*const fields = 
+[
+  {address:"street address",required:true},
+  {city:"city",required:true},
+  {zip :"zip code",required:true},
+  {state:"state",required:true}
+]*/
+
+/*export default class Screen extends React.Component {
+  
+  constructor() 
+  {
+    super();
+    this.state = 
+    {
+      formData:{}
+    };
+  }
+
+  onChange = (e, name, value)=> 
+  {
+    const formData = extend({},this.state.formData,{[name]:value});
+    this.setState({formData:formData});
+  }
+
+  /*click = (a) => {
+    alert(a);
+  }*/
+
+  /*constructor() {
+    super()
+    this.beacons.data = 
+    {
+      [
+        {
+          uuid: "3cfc4026-5e81-4b76-a682-d54b7e252d12",
+          major : 34,
+          minor : 402,
+          rssi : -72,
+          positions : //Only exists if includePos is "Y"
+          [ 
+            {
+              posCode : "42",
+              locRef : "ref",
+              desc : "Harrison House",
+              note : "This is the house on Harrison",
+              //img : "https://portal.campusm.exlibrisgroup.com/assets/image.jpg",
+              mapCode : "10",
+              locCode : "6",
+              locCatDesc : "Buildings",
+              houseName : "Harrison House",
+              flat : "Flat 1",
+              streetName : "Harrison",
+              district : "University Quarter",
+              town : "Lowell",
+              county : "Indiana",
+              postCode : "46356",
+              showOnMap : true,
+              longitude : "-87.405738",
+              latitude : "41.293544"   
+            }
+          ]
+        },
+
+        {
+          uuid: "3cfc4026-5e81-4b76-a682-d54b7e252d12",
+          major : 34,
+          minor : 401,
+          rssi : -72,
+          positions : //Only exists if includePos is "Y"
+          [ 
+            {
+              posCode : "43",
+              locRef : "ref1",
+              desc : "Harrison Backyard",
+              note : "This is the backyard of the house on Harrison",
+              //img : "https://portal.campusm.exlibrisgroup.com/assets/image.jpg",
+              mapCode : "10",
+              locCode : "6",
+              locCatDesc : "Buildings",
+              houseName : "Harrison House",
+              flat : "Flat back",
+              streetName : "Harrison",
+              district : "University Quarter",
+              town : "Lowell",
+              county : "Indiana",
+              postCode : "46356",
+              showOnMap : true,
+              longitude : "-87.405955",
+              latitude : "41.293681"   
+            }
+          ]
+        }
+      ]
+    }
+  
+     
+    
+  
+
 
   componentDidMount() {
     
   }
 
+
+
+
+
   render() {
 
-    beacons.on("data", (data) => {
-      setBeaconData(data);
-      alert("Beacons detected");
-    });
+    
 
-    return (
+    /*return (
       
-      
+      //only stuff inside the container tags is actually going to show up in the app
       <Container>
+
+
+
+
+
+
         <VBox>
+          <div className = "topStuff">
           <BannerHeader theme="alt" key="header" data-flex={0}>PNW Wayfinding App</BannerHeader>
 
-          <Button href="campusm://menu?menucode=1000005687">Click Me</Button>
+           <Button href="campusm://menu?menucode=1000005687">Click Me</Button>
 
-          <Field label="My Input"/>
+            
           
           
+          </div>
           
+          <div>
+          
+          <label></label>
+          </div>
         </VBox>
-        
+        <div>
+            
+          </div>
 
       
       </Container>
       
-    )
-  }
-
-}
+    )*/
+    
